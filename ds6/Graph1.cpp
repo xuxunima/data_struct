5fc4c1f345c811a7abf9907ad845e3d2a99b9556
Jialu Xu <xujialu801516@163.com>
2018-10-14 11:33:49 +0800

    添加了邻接矩阵的DFS和BFS

--- ds6/Graph1.cpp
+++ ds6/Graph1.cpp
@@ -1,4 +1,5 @@
 #include<iostream>
+#include<queue>
 using namespace std;
 
 typedef int WeightType;
@@ -22,14 +23,19 @@ struct GNode
 };
 typedef GNode * MGraph;
 
+bool visited[MAX_N] = { 0 };
 MGraph BuildGraph();
 MGraph CreateGraph(int N);
 void InsertEdge(MGraph Graph, Edge E);
+void Visit(Vertex v);
+bool isEdge(MGraph Graph, Vertex v1, Vertex v2);
+void BFS(MGraph Graph, Vertex v, void(*Visit)(Vertex));
+void DFS(MGraph Graph, Vertex v, void(*Visit)(Vertex));
 
 int main()
 {
 	MGraph Graph = BuildGraph();
-	cout << Graph->Nv << " " << Graph->Ne << endl;
+	DFS(Graph, 0, Visit);
 	return 0;
 }
 
@@ -75,4 +81,51 @@ MGraph BuildGraph()
 	for (Vertex i = 0; i < Graph->Nv; i++)
 		cin >> Graph->data[i];
 	return Graph;
+}
+
+void Visit(Vertex v)
+{
+	cout << "顶点" << v << "正在被访问" << endl;
+}
+
+bool isEdge(MGraph Graph, Vertex v1, Vertex v2)
+{
+	return Graph->G[v1][v2] < INFINIT ? true : false;
+}
+
+void BFS(MGraph Graph, Vertex v, void(*Visit)(Vertex))
+{
+	queue<Vertex>q;
+	Vertex w;
+	visited[v] = true;
+	Visit(v);
+	q.push(v);
+	while (!q.empty())
+	{
+		v = q.front();
+		q.pop();
+		for (w = 0; w < Graph->Nv; w++)
+		{
+			if (!visited[w] && isEdge(Graph, v, w))
+			{
+				visited[w] = true;
+				Visit(w);
+				q.push(w);
+			}
+		}
+	}
+}
+
+void DFS(MGraph Graph, Vertex v, void(*Visit)(Vertex))
+{
+	visited[v] = true;
+	Visit(v);
+	Vertex w;
+	for (w = 0; w < Graph->Nv; w++)
+	{
+		if (!visited[w] && isEdge(Graph, v, w))
+		{
+			DFS(Graph, w, Visit);
+		}
+	}
 }
